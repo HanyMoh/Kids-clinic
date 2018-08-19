4c8397e5147dc80256fb76956d719a80343392f8
Hany <hanymoh@msn.com>
2018-08-19 04:48:23 +0200

    landing Page mobile responcive

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,13 +5,29 @@
 //= require_tree .
 
 $(document).ready(function() {
+  $(".submenu > a").click(function(e) {
+    e.preventDefault();
+    var $li = $(this).parent("li");
+    var $ul = $(this).next("ul");
+
+    if ($li.hasClass("open")) {
+      $ul.slideUp(350);
+      $li.removeClass("open");
+    } else {
+      $(".nav > li > ul").slideUp(350);
+      $(".nav > li").removeClass("open");
+      $ul.slideDown(350);
+      $li.addClass("open");
+    }
+  });
+
   $('.datepicker').datepicker({
-      todayBtn: 'linked',
-      format: 'yyyy-mm-dd',
-      keyboardNavigation: false,
-      forceParse: false,
-      calendarWeeks: true,
-      autoclose: true,
-      todayHighlight: true,
-    });
+    todayBtn: 'linked',
+    format: 'yyyy-mm-dd',
+    keyboardNavigation: false,
+    forceParse: false,
+    calendarWeeks: true,
+    autoclose: true,
+    todayHighlight: true,
+  });
 });
